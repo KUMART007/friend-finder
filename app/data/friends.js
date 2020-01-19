e011645eeea88852d6708b5d55f3c668eab6514e
martin <martinezogo1987@gmail.com>
2020-01-18 22:53:16 -0600

    friend

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends=[{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Peter",
+    "photo":"https://healthyceleb.com/wp-content/uploads/2016/08/Peter-Facinelli-at-ComicCon-2015.jpg",
+    "scores":[
+        3,
+        5,
+        1,
+        2,
+        5,
+        1,
+        2,
+        3,
+        5,
+        1
+      ]
+  }]
+
+  module.exports = friends;
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT
-const htmlRoutes = require('./app/routing/htmlRoutes.js')
-const apiRoutes = require('./app/routing/apiRoutes.js')
+const PORT = process.env.PORT || 8080;
+// const htmlRoutes = require('./app/routing/htmlRoutes.js')
+// const apiRoutes = require('./app/routing/apiRoutes.js')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+
 app.use(express.static('css'))
 
-app.use('/api',apiRoutes)
-app.use('/',htmlRoutes)
+// app.use('/api',apiRoutes)
+// app.use('/',htmlRoutes)
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
-app.listen(PORT,function(){
-    console.log('App listening localhost:'+ PORT)
-})
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
