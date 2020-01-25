5d7aacc37f36329a687fd48225e2e6349e0fe146
martin <martinezogo1987@gmail.com>
2020-01-25 12:19:37 -0600

    more cleanup

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8080;
+const apiRoutes = require('./app/routing/apiRoutes.js');
+const htmlroutes = require('./app/routing/htmlroutes.js');
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
-
-
+apiRoutes(app);
+htmlroutes(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
